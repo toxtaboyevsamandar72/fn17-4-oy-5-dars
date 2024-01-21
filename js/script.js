c59360d37222ac1a1866b8bc6147b0b3292ff1c4
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-21 16:12:30 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,87 @@
+//  0-masal
+
+// for (let i = 1; i <=100; i++) {
+//     console.log(i);
+// }
+
+
+// 1-masala
+
+// let n = +prompt("soni kiriting");
+
+// for (let i = 1; i <= n; i++) {
+//    console.log(i);
+// }
+
+
+
+// 2-masala
+
+// 
+
+// let n = +prompt("soni kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//    sum = sum+i;
+// }
+// console.log(sum);
+
+
+// 3-masala
+
+
+
+// let n = +prompt("soni kiriting");
+
+// let sum = 0;
+// for (let i= 1; i <= n; i++) {
+//    if (i%2==1) {
+//       sum+=i;
+//    }
+// }
+// console.log(sum);
+
+
+
+
+// 4-masal
+
+
+
+// let a = +prompt("1 soni kiriting");
+// let b = +prompt("2 soni kiriting");
+
+// let sum = 1;
+// for (let i= a; i <= b; i++) {
+//    if (i%2==0) {
+//       sum=sum*i;
+//    }
+// }
+// console.log(sum);
+
+
+// 5-masal
+
+
+// let a = +prompt("1 soni kiriting");
+// let b = +prompt("2 soni kiriting");
+
+// let sum = 0;
+// for (let i= a; i <= b; i++) {
+//    if (i%3==0 && i%5==0) {
+//       sum+=i;
+//    }
+// }
+// console.log(sum);
+
+
+// 6-masal
+
+
+// let num = +prompt("soni kiriting");
+// let sum = 0;
+
+// if (num%2==0) {
+//    sum++;
+// }
+// console.log(sum);
